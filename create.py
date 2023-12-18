01c1096e024a7dc1e86c0cb2f42ab7d483412f3f
Ian Hunter <1540737+ianfhunter@users.noreply.github.com>
2023-12-18 16:47:29 +0000

    remove align

--- create.py
+++ create.py
@@ -14,24 +14,25 @@ def create_blocks():
         
                  
     with open(os.path.join(script_dir, "_doc/20_Creatures/21_Blocks.md"), "w") as f:
-        f.write("""
-            ---
-            title: Creature "Blocks"
-            category: Creatures
-            ---
+        f.write("""---
+title: Creature "Blocks"
+category: Creatures
+---
         """)
         for b in blocks: 
             print(f"Processing '{b['name']}'...")
             f.write(f"""
-            **Name:** {b['name']}
-             
-            | --------- | ----- |
-            | Health    | {b['stats']['health']} |
-            | Attack    | {b['stats']['attack']} |
-            | Defence    | {b['stats']['defence']} |
-    
-            **Tags:** {', '.join(b['tags'] if b['tags'] is not None else "")}
-            **Tactics:** {b['tactics']}
+**Name:** {b['name']}
+ 
+| --------- | ----- |
+| Health    | {b['stats']['health']} |
+| Attack    | {b['stats']['attack']} |
+| Defence    | {b['stats']['defence']} |
+
+**Tags:** {', '.join(b['tags'] if b['tags'] is not None else "")}
+**Tactics:** {b['tactics']}
+
+---
             """)
 
 def main():
